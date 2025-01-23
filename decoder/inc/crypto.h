6008dafaca351a607246310153dc4ae0ffa84a64
Vincent Wang <vinowang921@gmail.com>
2025-01-22 22:06:39 -0800

    Add basic crypto functions

---
+++ decoder/inc/crypto.h
@@ -0,0 +1,32 @@
+#ifndef ECTF_CRYPTO_H
+#define ECTF_CRYPTO_H
+
+#include "wolfssl/wolfcrypt/aes.h"
+#include "wolfssl/wolfcrypt/hash.h"
+
+/** @brief Decrypts ciphertext using a symmetric cipher
+ *
+ * @param ciphertext A pointer to a buffer of length len containing the
+ *           ciphertext to decrypt
+ * @param len The length of the ciphertext to decrypt. Must be a multiple of
+ *           BLOCK_SIZE (16 bytes)
+ * @param key A pointer to a buffer of length KEY_SIZE (16 bytes) containing
+ *           the key to use for decryption
+ * @param plaintext A pointer to a buffer of length len where the resulting
+ *           plaintext will be written to
+ *
+ * @return 0 on success, -1 on bad length, other non-zero for other error
+ */
+int decrypt_sym(uint8_t *ciphertext, size_t len, uint8_t *key, uint8_t *plaintext);
+
+/** @brief Hashes arbitrary-length data
+ *
+ * @param data A pointer to a buffer of length len containing the data
+ *           to be hashed
+ * @param len The length of the plaintext to hash
+ * @param hash_out A pointer to a buffer of length HASH_SIZE (16 bytes) where the resulting
+ *           hash output will be written to
+ *
+ * @return 0 on success, non-zero for other error
+ */
+int hash(void *data, size_t len, uint8_t *hash_out);
