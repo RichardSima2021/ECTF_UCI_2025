c2e048385e35b41be1682afaecd7665ceee53a67
Vincent Wang <vinowang921@gmail.com>
2025-01-27 13:48:54 -0800

    Run simple crypto example on Maxim and receive output

--- decoder/inc/simple_crypto.h
+++ decoder/inc/simple_crypto.h
@@ -10,7 +10,7 @@
  *
  * @copyright Copyright (c) 2025 The MITRE Corporation
  */
-
+#if CRYPTO_EXAMPLE
 #ifndef ECTF_CRYPTO_H
 #define ECTF_CRYPTO_H
 
@@ -67,4 +67,5 @@ int decrypt_sym(uint8_t *ciphertext, size_t len, uint8_t *key, uint8_t *plaintex
  */
 int hash(void *data, size_t len, uint8_t *hash_out);
 
-#endif // CRYPTO_EXAMPLE_SIMPLE_CRYPTO_H
+#endif // CRYPTO_EXAMPLE
+#endif // ECTF_CRYPTO_H
