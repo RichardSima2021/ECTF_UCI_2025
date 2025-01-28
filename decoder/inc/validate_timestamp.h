b3c3b8bd7ca1d1007dd9d77a9b4fdcb048ff2365
Jinyao <jinyaox@uci.edu>
2025-01-28 14:53:40 -0800

    50% Validate: Waiting on hardware flash read and write

---
+++ decoder/inc/validate_timestamp.h
@@ -0,0 +1,3 @@
+#include "decoder.h"
+
+int validate_timestamp(int channel_id, timestamp_t plaintext_ts, timestamp_t extracted_timestamp);
--- decoder/src/validate_timestamp.c
+++ decoder/src/validate_timestamp.c
@@ -5,28 +5,39 @@
 # define TIMESTAMP_LENGTH 8;
 
 
-int check_two_timestamp(char* plaintext_ts, char* extracted_timesamp){
+int check_two_timestamp(timestamp_t plaintext_ts, timestamp_t extracted_timestamp){
     //do a memcmp and return 0 if the result is correct, else return 1
+    // Convert the extracted timestamp to unit64_t
+    uint64_t extracted_ts = 0;
 
+    // Compare timestamps, return 0 if they match, 1 otherwise
+    int result=memcmp(&plaintext_ts, &extracted_timestamp, TIMESTAMP_LENGTH);
+    return result;
 }
 
-int check_increasing(int channel_id, char* extracted_timesamp){
+
+int check_increasing(int channel_id, timestamp_t extracted_timesamp){
     uint64_t extract_ts=0;
+    uint64_t current_ts=0;
     memcpy(&extract_ts,extracted_timesamp,8);
 
     //1. extract the channel_status strcuture from the flash
 
 
     //2. check if the timestamp is strictly greater than that
+
 }
 
 
 
-int validate_timestamp(int channel_id, uint64_t plaintext_ts, char* extracted_timesamp){
-    //Do a nested if
-
+int validate_timestamp(int channel_id, timestamp_t plaintext_ts, timestamp_t extracted_timestamp){
     //if check two are good, then another if check increasing, if both ok, return 0, else return 1
-    //stub
-    return 0;
+    if (check_two_timestamp(plaintext_ts, extracted_timestamp) == 0){
+        if (check_increasing(channel_id, extracted_timestamp) == 0){
+            return 0
+        }
+    }
+    //else
+    return 1;
 
 }
