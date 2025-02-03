c647d173c4044acd4b196e7a97a3b393282ebe21
leo <hermosilloleoncio@gmail.com>
2025-02-03 14:22:25 -0800

    pointer allocation change

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -246,7 +246,7 @@ int update_subscription(pkt_len_t pkt_len, encrypted_update_packet *packet) {
     decrypt_sym(&interwoven_encrypted, 48, channel_secrets->subscription_key, &interwoven_decrypted);
 
     // 4. & 5.
-    subscription_update_packet_t *update = malloc(sizeof(subscription_update_packet_t));
+    subscription_update_packet_t *update;
     update->channel = channel_id;
 
     unsigned char checksum [20];
