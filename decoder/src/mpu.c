1ea72e74d484070eb21a92983f1600c94733b2b1
Vincent Wang <vinowang921@gmail.com>
2025-03-10 13:38:41 -0700

    Implement MPU access protection
    Co-authored-by: Yintong Luo <yintong16@users.noreply.github.com>
    Co-authored-by: Dhruv Kandula <Drime648@users.noreply.github.com>

--- decoder/src/mpu.c
+++ decoder/src/mpu.c
@@ -1,20 +1,24 @@
 #include "mpu.h"
 #include <stdint.h>
+#include <stdio.h>
 
-void SVC_Handler(void) {
-    __set_CONTROL(__get_CONTROL() & ~0x1);
-    __ISB();
-}
+#include "host_messaging.h"
 
-void request_privilege() {
-    __asm("svc #0");
-}
 
-void drop_privilege() {
-    __set_CONTROL(__get_CONTROL() | 0x1);
-    __ISB();
-}
+int update_subscription(pkt_len_t pkt_len, encrypted_update_packet *packet);
+int update_current_timestamp(int channel_id, timestamp_t new_timestamp);
+int read_secrets(int channel_id, secret_t* secret_buffer);
 
+#define REQUEST_PRIVILEGE_IN_PRIVILEGED_READ_OFFSET (flash_privileged_read + 16) // placeholder
+#define REQUEST_PRIVILEGE_IN_PRIVILEGED_WRITE_OFFSET (flash_privileged_write + 18) // placeholder
+
+#define PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS (read_secrets + 90) // TODO, placeholder currently
+
+#define PRIVILEGED_WRITE_IN_UPDATE_SUBSCRIPTION_ADDRESS (update_subscription + 280) // TODO, placeholder
+
+#define SVC_HANDLER_IN_REQUEST_PRIVILEGE_OFFSET (request_privilege + 22)
+
+#undef CONDITIONAL_PRIV_ESCALATION_ENABLED
 
 /**
 * @brief    Setup the MPU
@@ -28,29 +32,43 @@ uint8_t mpu_setup() {
     // Ensure MPU is disabled
     MPU->CTRL = 0;
 
-    // Check if MPU present, return error if not
-    if (MPU->TYPE != 0) {
-        return 1;
-    }
-
-    int disabled_regions[] = {1, 2, 3, 4, 5};
-    int arr_size = 5;
+    int disabled_regions[] = {3, 4};
+    int arr_size = 2;
     // Background Region
     MPU->RNR = 0;
     MPU->RBAR = MXC_ROM_MEM_BASE;
-    MPU->RASR = (MPU_DEFS_RASR_SIZE_4GB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_FULL_ACCESS | MPU_RASR_ENABLE_Msk);
+    MPU->RASR = (MPU_DEFS_RASR_SIZE_4GB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_FULL_ACCESS | MPU_RASR_ENABLE_Msk | MPU_EXECUTION_DISABLE);
     //MPU->RASR = (MPU_DEFS_RASR_SIZE_4GB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_NO_ACCESS | MPU_RASR_ENABLE_Msk);
+
+    // // SRAM Flash Code Region
+    MPU->RNR = 1;
+    MPU->RBAR = MXC_SRAM_MEM_BASE;
+    MPU->RASR = (MPU_DEFS_RASR_SIZE_32KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_FULL_ACCESS | MPU_RASR_ENABLE_Msk); // | MPU_EXECUTION_DISABLE);
+
+    // FLASH TEXT Region
+    MPU->RNR = 2;
+    MPU->RBAR = MXC_FLASH_MEM_BASE;
+    MPU->RASR = (MPU_DEFS_RASR_SIZE_256KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_RO | MPU_RASR_ENABLE_Msk);
+
+    // Peripheral Region
+    // MPU->RNR = 3;
+    // MPU->RBAR = 
+
+    // Secrets Overlay Region
+    MPU->RNR = 5;
+    MPU->RBAR = MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE - 4 * MXC_FLASH_PAGE_SIZE;
+    MPU->RASR = (MPU_DEFS_RASR_SIZE_32KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_NO_ACCESS | MPU_RASR_ENABLE_Msk);// | MPU_EXECUTION_DISABLE
+
+    // Decoder Status Region
+    MPU->RNR = 6;
+    MPU->RBAR = MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE - 2 * MXC_FLASH_PAGE_SIZE;
+    MPU->RASR = (MPU_DEFS_RASR_SIZE_4KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_PRIV_RW_USER_RO | MPU_RASR_ENABLE_Msk);// | MPU_EXECUTION_DISABLE
 
     // Secrets Region
     MPU->RNR = 7;
     MPU->RBAR = MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE - 3 * MXC_FLASH_PAGE_SIZE; 
-    //MPU->RASR = (MPU_DEFS_RASR_SIZE_8KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_PRIV_RO | MPU_RASR_ENABLE_Msk);
-    MPU->RASR = (MPU_DEFS_RASR_SIZE_8KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_RO | MPU_RASR_ENABLE_Msk);
-
-    // Secrets Overlay Region
-    MPU->RNR = 6;
-    MPU->RBAR = MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE - 4 * MXC_FLASH_PAGE_SIZE;
-    MPU->RASR = (MPU_DEFS_RASR_SIZE_32KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_NO_ACCESS | MPU_RASR_ENABLE_Msk);
+    MPU->RASR = (MPU_DEFS_RASR_SIZE_4KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_PRIV_RO | MPU_RASR_ENABLE_Msk);// | MPU_EXECUTION_DISABLE
+    //MPU->RASR = (MPU_DEFS_RASR_SIZE_8KB | MPU_DEFS_NORMAL_MEMORY_WT | MPU_DEFS_RASE_AP_RO | MPU_RASR_ENABLE_Msk);
 
     // Disable unused regions
     for (i = 0; i < arr_size; ++i) {
@@ -70,13 +88,104 @@ uint8_t mpu_setup() {
     return 0;
 }
 
-void flash_privileged_read(uint32_t address, void *buffer, uint32_t len) {
+/**
+ * @brief SVC Handler
+ * @details This function is called when an SVC interrupt is triggered. It clears the control bit for privileged mode.
+ */
+__attribute__((noinline)) void SVC_Handler(void) {
+    __set_CONTROL(__get_CONTROL() & ~0x1);
+    __ISB();
+
+#ifdef CONDITIONAL_PRIV_ESCALATION_ENABLED
+    __asm volatile(
+        "TST lr, #4 \n"              // Test EXC_RETURN bit 2 (indicates which stack is used)
+        "ITE EQ \n"
+        "MRSEQ r0, MSP \n"           // If 0, use Main Stack Pointer (MSP)
+        "MRSNE r0, PSP \n"           // If 1, use Process Stack Pointer (PSP)
+        "B svc_handler_c \n"         // Branch to C handler
+    );
+#endif
+} 
+
+#ifdef CONDITIONAL_PRIV_ESCALATION_ENABLED
+__attribute__((noinline)) void svc_handler_c(uint32_t *stack_frame) {
+    void* return_addr = stack_frame[6];
+    char buf[20];
+    sprintf(buf, "Ret Addr:   0x%.8x\n", return_addr);
+    print_debug(buf);
+    sprintf(buf, "Check Addr: 0x%.8x\n", SVC_HANDLER_IN_REQUEST_PRIVILEGE_OFFSET);
+    print_debug(buf);
+    printf("!!!!!!!!!!! 0x.8%x !!!!!!!!!!!!\n", return_addr);
+    if ((return_addr+1) != SVC_HANDLER_IN_REQUEST_PRIVILEGE_OFFSET) {
+        print_debug("Invalid SVC call, crashing...\n");
+        while(1);
+    }
+    print_debug("Successful SVC call\n");
+}
+#endif
+
+/**
+ * @brief request privilege
+ * @details This function checks if the return address is the flash_privileged_read function 
+ *          if it is, it requests privilege by calling the SVC interrupt. It will crash if its not the correct return address.
+ */
+__attribute__((noinline))
+void request_privilege() {
+#ifdef CONDITIONAL_PRIV_ESCALATION_ENABLED
+    void* return_addr = __builtin_return_address(0);
+    if(return_addr != REQUEST_PRIVILEGE_IN_PRIVILEGED_READ_OFFSET &&
+       return_addr != REQUEST_PRIVILEGE_IN_PRIVILEGED_WRITE_OFFSET){
+        // print_error("Failing in request_privilege");
+        while (1);
+       }
+#endif
+    __asm("svc #0");
+}
+
+
+/**
+ * @brief Disable Privilege mode
+ * @details This function disables the privilege mode by setting the CONTROL bit.
+ *          It should be called immediately after the operation that requires privilege.
+*/
+void drop_privilege() {
+    __set_CONTROL(__get_CONTROL() | 0x1);
+    __ISB();
+}
+
+
+/**
+ * @brief read from flash in privileged mode
+ * @details if the return address is correct, switch to privileged mode, if not crash, if privilege mode is enabled,
+ *          read from flash, and then drop privilege
+ */
+__attribute__((noinline)) void flash_privileged_read(uint32_t address, void *buffer, uint32_t len) {
+#ifdef CONDITIONAL_PRIV_ESCALATION_ENABLED
+    void* return_addr = __builtin_return_address(0);
+    if(return_addr != PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS){
+        // print_error("Failing in flash_privileged_read");
+        while (1);
+    }
+#endif
     request_privilege();
+
     flash_read(address, buffer, len);
     drop_privilege();
 }
 
-int flash_privileged_write(uint32_t address, void* buffer, uint32_t len) {
+/**
+ * @brief write to flash in privileged mode
+ * @details if the return address is correct, switch to privileged mode, if not crash, if privilege mode is enabled,
+ *          write to flash, and then drop privilege
+ */
+__attribute__((noinline)) int flash_privileged_write(uint32_t address, void* buffer, uint32_t len) {
+#ifdef CONDITIONAL_PRIV_ESCALATION_ENABLED
+    void* return_addr = __builtin_return_address(0); 
+    if ((return_addr != PRIVILEGED_WRITE_IN_UPDATE_SUBSCRIPTION_ADDRESS)){
+        // print_error("Failing in flash_privileged_write");
+        while (1);
+    }
+#endif
     request_privilege();
     int error = flash_write(address, buffer, len);
     drop_privilege();
