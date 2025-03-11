e1372bb90c714016b7c18c3f3b4fb23f2fdf5f9a
zhanhaoruan <zhanhaoruan@gmail.com>
2025-03-11 13:30:06 -0700

    binary search on return address
    Co-Authored-By: Gensen Xiao <154722375+Mintbroke@users.noreply.github.com>
    Co-Authored-By: Leo <114712210+LeoHerms@users.noreply.github.com>
    Co-Authored-By: Richard <63275399+RichardSima2021@users.noreply.github.com>
    Co-Authored-By: Jinyaox <103966714+Jinyaox@users.noreply.github.com>

--- decoder/src/mpu.c
+++ decoder/src/mpu.c
@@ -178,7 +178,7 @@ void flash_privileged_read(uint32_t address, void *buffer, uint32_t len) {
     /*sprintf(buf, "Check Addr offset of PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS: %d\n", PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS - read_secrets);*/
     /*print_debug(buf);*/
 
-    if(!(PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS <= return_addr && return_addr <= PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS + 8)) {
+    if(!(PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS - 32 <= return_addr && return_addr <= PRIVILEGED_READ_IN_READ_SECRETS_ADDRESS + 32)) {
         // print_error("Failing in flash_privileged_read");
         while (1);
     }
